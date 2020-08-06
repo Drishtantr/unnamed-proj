7f37121222bfda1e8bcdaf2b801eac45909a196d
Prasen Shakya <kajiprasen@gmail.com>
2020-08-06 17:35:22 +0545

    Added test code

---
+++ src/components/Byeworld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    Bye Bye world
+
+    <span ref="floatwindow">{{ count }}</span>
+    <button @click="openWindow">Click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Byeworld',
+  data: () => ({
+    count: 1,
+  }),
+  methods: {
+    increase() {
+      this.count += 1;
+    },
+  },
+  created() {
+    alert('Hello');
+  },
+};
+</script>
+
+<style scoped>
+div {
+  background-color: red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,29 +5,28 @@ import router from './router';
 Vue.config.productionTip = false;
 
 /* below function to DISPLAY floating window on screen when Or YouTube Video button pressed */
-function openWindow() {
-  const x = document.getElementById('floatwindow');
-  y = x.style.display = 'block';
-  return y;
-}
+// function openWindow() {
+//   const x = document.getElementById('floatwindow');
+//   y = x.style.display = 'block';
+//   return y;
+// }
 
-/* below function to HIDE floating window on screen when cross button is pressed */
-function closeWindow() {
-  const x = document.getElementById('floatwindow');
-  y = x.style.display = 'none';
-  return y;
-}
+// /* below function to HIDE floating window on screen when cross button is pressed */
+// function closeWindow() {
+//   const x = document.getElementById('floatwindow');
+//   y = x.style.display = 'none';
+//   return y;
+// }
 
-function videoclick(obj) {
-  document.getElementById('mycontainer').style.display = 'inline';
-  const myvideo = document.getElementById('match');
-  const reader = new FileReader();
-  reader.onload = (function (video) { return function (e) { video.src = e.target.result; }; }(myvideo));
-  reader.addEventListener('load', () => {
-    myvideo.play();
-  });
-  reader.readAsDataURL(obj.files[0]);
-}
+// function videoclick(obj) {
+//   document.getElementById('mycontainer').style.display = 'inline';
+//   const myvideo = document.getElementById('match');
+//   const reader = new FileReader();
+//   reader.addEventListener('load', () => {
+//     myvideo.play();
+//   });
+//   reader.readAsDataURL(obj.files[0]);
+// }
 
 new Vue({
   router,
